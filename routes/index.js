e456917a5709f528bc10a016f1751bdbae344004
Andres Padilla <anarpafran@gmail.com>
2014-10-25 18:27:01 -0500

    Logo + contenido del landing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Esperimenta una forma diferente de gestionar turnos' });
+  res.render('index', { title: 'Turnos, Facil y Rapido' });
 });
 
 module.exports = router;
