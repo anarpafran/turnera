8569e52783e2d340b8b95ec3f5ae21eebfbe0670
Andres Padilla <anarpafran@gmail.com>
2014-10-26 11:11:01 -0500

    Se compreta diseño de landing

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  	var user = req.query; 
+  	console.log(user);
+  	var fs = require('fs');
+	var line = user.name+","+user.last+","+user.mail+"\n";
+	fs.readFile("db.txt", function(err, data){
+		if(err){
+			console.log("Error File: "+err);
+			console.log(line);
+		}else{
+		
+			fs.writeFile("db.txt", data + line ,function(err){
+				if(err){
+					console.log("Error File: "+err);
+					console.log(line);
+				}
+
+			});
+		}
+	});
+
+	 
+	
+
+  	res.render('index', { err: "", title: 'Turnos, Facil y Rapido', send: ""});
 });
 
 module.exports = router;
